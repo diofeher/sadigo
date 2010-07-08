d90fa25abeb91d6d9c687ba55d20261acf3c5512
diofeher <diofeher@3e0cf7fb-61e7-7ffb-15fb-1319d0c82416>
2010-07-08 15:38:31 +0000

    made a (dirty) login system.

--- forms.py
+++ forms.py
@@ -8,7 +8,7 @@ Copyright (c) 2010 __MyCompanyName__. All rights reserved.
 """
 
 from flask import Flask
-from flaskext.fungiform import Form, TextField
+from flaskext.fungiform import Form, TextField, ValidationError
 
 app = Flask(__name__)
 app.secret_key = 'bolas'
@@ -16,3 +16,10 @@ app.secret_key = 'bolas'
 class LoginForm(Form):
     login = TextField(required=True)
     password = TextField(required=True)
+    
+    def context_validate(self, data):
+        if data['login'] == 'admin':
+            if data['password'] != 'admin':
+                raise ValidationError(u'Password incorrect')
+        else:
+            raise ValidationError(u'Unknown user')
--- sadigo.py
+++ sadigo.py
@@ -7,7 +7,7 @@ Created by Diogenes Herminio on 2010-06-24.
 Copyright (c) 2010 __MyCompanyName__. All rights reserved.
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session, url_for, request, redirect, flash
 from forms import LoginForm
 
 app = Flask(__name__)
@@ -17,14 +17,25 @@ app.secret_key = 'bolas'
 def index():
     return render_template('index.html')
   
+@app.route('/admin') 
+def admin():
+    return render_template('admin.html')
+
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     form = LoginForm()
+    if request.method == 'POST':
+        form = LoginForm()
+        if form.validate():
+            session.logged_in = True
+            flash('Logged with success', category='message')
+            return redirect(url_for('admin'))
     return render_template('login.html', form=form.as_widget())
     
 @app.route('/logout') 
 def logout():
-    pass
+    session.pop('logged_in', None)
+    return url_for('index')
     
 if __name__ == "__main__":
     app.run(debug=True)
