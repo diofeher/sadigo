b181bf1b28f91e65c07fc91df3c9f14393aa1557
diofeher <diofeher@3e0cf7fb-61e7-7ffb-15fb-1319d0c82416>
2010-07-08 05:24:03 +0000

    added login form and change app structure

--- trunk/sadigo.py
+++ trunk/sadigo.py
@@ -1,9 +1,30 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+models.py
 
-@app.route("/")
+Created by Diogenes Herminio on 2010-06-24.
+Copyright (c) 2010 __MyCompanyName__. All rights reserved.
+"""
+
+from flask import Flask, render_template
+from forms import LoginForm
+
+app = Flask(__name__)
+app.secret_key = 'bolas'
+
+@app.route('/')
 def index():
     return render_template('index.html')
-        
+  
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form.as_widget())
+    
+@app.route('/logout') 
+def logout():
+    pass
+    
 if __name__ == "__main__":
     app.run(debug=True)
